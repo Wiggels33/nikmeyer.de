2829e0830c25f2096e97d2790cacfa24eda5a46b
Niklas Meyer <>
2025-05-15 20:57:11 +0200

    set up Sections

---
+++ app/sections/contactSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../ui/container";
+
+
+const ContactSection = () => {
+
+    return (
+        <section>
+            <Container>ContactSection</Container>
+        </section>
+    );
+}
+
+export default ContactSection;
